038839bcdc4ee38d8b7c696180620c91b9f9c848
IvanGM <ivangm_01@hotmail.com>
2023-03-17 00:33:10 -0600

    Sección de comentarios añadida

--- Prueba.js
+++ Prueba.js
@@ -1,3 +1,5 @@
+
+
 function myFunction(botonid, solid) {
     var x = document.getElementById(solid);
     var y = document.getElementById(botonid);
@@ -13,3 +15,23 @@ function myFunction(botonid, solid) {
       y.classList.add('b2')
     }
   }
+
+
+function addElement() {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  newDiv.classList.add('comentarios')
+
+  // and give it some content
+  const newContent = document.createTextNode(document.getElementById('input').value);
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("Comentarios");
+  document.body.insertBefore(newDiv, currentDiv.nextSibling);
+  document.getElementById('input').value=''
+}
+
