b551d4e6cef2516a5a72a8b3c528f70357c1732a
IvanGM <ivangm_01@hotmail.com>
2023-03-28 19:54:01 -0600

    Article lateral completo

--- YouTube.js
+++ YouTube.js
@@ -1,7 +1,17 @@
 function buscar(){
     var busqueda=document.getElementById('search').value;
+    if(busqueda){
     busqueda=busqueda.replace(" ", "+")
     busqueda='https://www.youtube.com/results?search_query='+busqueda
-    document.getElementById('search').value=''
     window.open(busqueda)
+    document.getElementById('search').value=''
+    }
 }
+
+document.getElementById("search")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+        document.getElementById("search_button").click();
+    }
+});
