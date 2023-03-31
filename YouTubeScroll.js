837853fe6a031fbdcfb878a52eeada7a73737b37
IvanGM <ivangm_01@hotmail.com>
2023-03-31 00:50:49 -0600

    El archivo YouTubeScroll contiene lo necesario para la barra de navegación con carrusel

---
+++ YouTubeScroll.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    margin: 20,
+    nav: true,
+    dots: false,
+    navText:["<div class='nav-btn prev-slide'><img src='arrow_left.png' style='width: 12px; height: 12px' title='Anterior'></div>","<div class='nav-btn next-slide'><img src='arrow_right.png' style='width: 12px; height: 12px' title='Siguiente'></div>"],
+    responsive: {
+        0: {
+            slideBy: 1,
+            items: 1
+        },
+        600: {
+            slideBy: 2,
+            items: 3
+        },
+        1000: {
+            slideBy: 5,
+            items: 6
+        }
+    }
+    });
