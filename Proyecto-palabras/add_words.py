c8bf9afcf55361e6ba9b597217c9259d049db0f9
IvanGM <ivangm_01@hotmail.com>
2023-06-08 22:37:25 -0600

    Estilos de meaning modificados y palabras añadidas

--- Proyecto-palabras/add_words.py
+++ Proyecto-palabras/add_words.py
@@ -7,25 +7,33 @@ while seguir:
     print('Ingresa 1 si quieres añadir una nueva palabra, de lo contrario ingresa enter')
     a = input()
     if a=="1":
-        item = {"word": "", "meaning": "", "spanish": ""}
-        item["word"] = input('word: ')
-        meaning = input('meaning: ')
-        if meaning[-1] != ".":
-            meaning = meaning +"."
-        item["meaning"] = meaning
-        item["spanish"] = input('spanish: ')
-        arr = arr + [item]
+        try:
+            item = {"word": "", "meaning": "", "spanish": ""}
+            item["word"] = input('word: ')
+            meaning = input('meaning: ')
+            if meaning[-1] != ".":
+                meaning = meaning +"."
+            item["meaning"] = meaning.capitalize()
+            item["spanish"] = input('spanish: ')
+            arr = arr + [item]
+        except:
+            print('Hubo un error al añadir la palabra')
     else:
         seguir = False
 print(arr)
 print('Ingresa 1 si deseas añadirlo al diccionario actual, de lo contrario ingresa enter')
 b = input()
 if b=="1":
-    filename = "C:\\Users\\Lenovo\\Documents\\HTMLDevF\\Proyectos-personales\\Proyecto-palabras\\api\\words.json"
-    with open(filename, "r") as file:
-        data = json.load(file)
-    for newWord in arr:
-        data.append(newWord)
-    with open(filename, "w") as file:
-        json.dump(data, file)
-    print('Se ha terminado de añadir')
+    try:
+        filename = "C:\\Users\\Lenovo\\Documents\\HTMLDevF\\Proyectos-personales\\Proyecto-palabras\\api\\words.json"
+        with open(filename, "r") as file:
+            data = json.load(file)
+        for newWord in arr:
+            data.append(newWord)
+        with open(filename, "w") as file:
+            json.dump(data, file)
+        print('Se ha terminado de añadir')
+    except:
+        print('No se pudo agregar al diccionario')
+else:
+    print('El diccionario no ha sido modificado')
